3251e21881d176616358811f1d840b5102dd8485
976778267@qq.com <>
2019-10-15 14:09:54 +0800

    - [modify] 添加lombok支持

--- litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/vo/CouponVo.java
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/vo/CouponVo.java
@@ -1,7 +1,12 @@
 package org.linlinjava.litemall.wx.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 public class CouponVo {
     private Integer id;
     private String name;
@@ -11,68 +16,4 @@ public class CouponVo {
     private String discount;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public String getTag() {
-        return tag;
-    }
-
-    public void setTag(String tag) {
-        this.tag = tag;
-    }
-
-    public String getMin() {
-        return min;
-    }
-
-    public void setMin(String min) {
-        this.min = min;
-    }
-
-    public String getDiscount() {
-        return discount;
-    }
-
-    public void setDiscount(String discount) {
-        this.discount = discount;
-    }
-
-    public LocalDateTime getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(LocalDateTime startTime) {
-        this.startTime = startTime;
-    }
-
-    public LocalDateTime getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(LocalDateTime endTime) {
-        this.endTime = endTime;
-    }
 }
--- litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/vo/GrouponRuleVo.java
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/vo/GrouponRuleVo.java
@@ -1,7 +1,12 @@
 package org.linlinjava.litemall.wx.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Getter
+@Setter
 public class GrouponRuleVo {
     private Integer id;
     private String name;
@@ -12,76 +17,4 @@ public class GrouponRuleVo {
     private BigDecimal grouponPrice;
     private BigDecimal grouponDiscount;
     private Integer grouponMember;
-
-    public BigDecimal getGrouponDiscount() {
-        return grouponDiscount;
-    }
-
-    public void setGrouponDiscount(BigDecimal grouponDiscount) {
-        this.grouponDiscount = grouponDiscount;
-    }
-
-    public Integer getGrouponMember() {
-        return grouponMember;
-    }
-
-    public void setGrouponMember(Integer grouponMember) {
-        this.grouponMember = grouponMember;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBrief() {
-        return brief;
-    }
-
-    public void setBrief(String brief) {
-        this.brief = brief;
-    }
-
-    public String getPicUrl() {
-        return picUrl;
-    }
-
-    public void setPicUrl(String picUrl) {
-        this.picUrl = picUrl;
-    }
-
-    public BigDecimal getCounterPrice() {
-        return counterPrice;
-    }
-
-    public void setCounterPrice(BigDecimal counterPrice) {
-        this.counterPrice = counterPrice;
-    }
-
-    public BigDecimal getRetailPrice() {
-        return retailPrice;
-    }
-
-    public void setRetailPrice(BigDecimal retailPrice) {
-        this.retailPrice = retailPrice;
-    }
-
-    public BigDecimal getGrouponPrice() {
-        return grouponPrice;
-    }
-
-    public void setGrouponPrice(BigDecimal grouponPrice) {
-        this.grouponPrice = grouponPrice;
-    }
 }
